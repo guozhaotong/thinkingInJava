c5eec679ec72629f388c1f6a470cf63f51334ce3
guozhaotong <xjtuguozhaotong@qq.com>
2017-12-04 22:05:10 +0800

    16.2

---
+++ src/main/java/chapter15/GenericsAndReturnTypes.java
@@ -0,0 +1,23 @@
+package chapter15;
+
+
+/**
+ * @author 郭朝彤
+ * @date 2017/12/4.
+ */
+
+interface GenericGetter <T extends GenericGetter<T>>{
+    T get();
+}
+
+interface Getter extends GenericGetter<Getter>{}
+
+public class GenericsAndReturnTypes {
+    void test(Getter g){
+        Getter result = g.get();
+        GenericGetter genericGetter = g.get();
+    }
+    public static void main(String[] args){
+
+    }
+}
